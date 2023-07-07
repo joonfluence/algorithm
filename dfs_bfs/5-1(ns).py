d1d45db301dba9618526de0ff3df955d09734c7e
joonho lee <joonfluence.dev@gmail.com>
2023-07-07 17:19:46 +0900

    [Chapter5] DFS & BFS

---
+++ dfs_bfs/5-1(ns).py
@@ -0,0 +1,11 @@
+def dfs(graph, v, visited):
+  visited[v] = True
+  print(v, end=' ')
+  for i in graph[v]:
+    if not visited[i]:
+      dfs(graph, i, visited)
+
+graph = [[], [2,3,8], [1,7], [1,4,5], [3,5], [3,4], [7], [2,6,8], [1,7]] # 인접리스트 방식
+
+visited = [False] * 9
+dfs(graph, 1, visited)
