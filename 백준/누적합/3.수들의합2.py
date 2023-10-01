de05c21e5ec903123d852c03834d5fbc084ff0ba
joonho lee <joonfluence.dev@gmail.com>
2023-10-01 21:56:44 +0900

    누적합 - 수들의합

---
+++ 백준/누적합/3.수들의합2.py
@@ -0,0 +1,18 @@
+n, m = list(map(int, input().split()))
+numbers = list(map(int, input().split()))
+
+left, right = 0, 1
+count = 0
+
+while left <= right and right <= n:
+  total = sum(numbers[left:right])
+  
+  if total == m:
+    count += 1
+    right += 1
+  elif total < m:
+    right += 1
+  else:
+    left += 1
+
+print(count)
