873899a6dd1965bbf085d49cd233b8db588af6e8
joonho lee <joonfluence.dev@gmail.com>
2024-01-31 12:29:43 +0900

    (브론즈5) 구현 1330

--- 백준/구현/IM_1330.java
+++ 백준/구현/IM_1330.java
@@ -1,2 +1,25 @@
-package 구현;public class IM_1330 {
+package 구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class IM_1330 {
+    private final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        Integer[] nums = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+
+        int A = nums[0];
+        int B = nums[1];
+
+        if(A == B){
+            System.out.println("==");
+        } else if(A < B){
+            System.out.println("<");
+        } else if(A > B){
+            System.out.println(">");
+        }
+    }
 }
